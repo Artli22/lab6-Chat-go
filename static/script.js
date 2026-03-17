1cf1c1efb5ffbf7c257ca49899a4f417c4e4ee80
Artli22 <ludinlima2514@gmail.com>
2026-03-17 14:07:30 -0600

    Cambios esteticos parte 1

--- static/script.js
+++ static/script.js
@@ -55,3 +55,11 @@ const getMessages = async () =>{
         textarea.value = ''
         counter.textContent = '0/140'
     })
+
+function toggleTheme() {
+  document.documentElement.classList.toggle('dark');
+  document.documentElement.style.colorScheme = 
+  document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+}
+
+window.toggleTheme = toggleTheme;
